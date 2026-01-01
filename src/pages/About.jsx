b5e29638cc2caa74d9ae9a52826cda5e593800ba
[NilaBeast] <[manika.basak1977@gmail.com]>
2026-01-01 15:49:55 +0530

    initial commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,7 +31,7 @@ export default function About() {
 
         {/* Content */}
         <h2  className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
-          About Mini World of Alice
+          About Miniworld of ALICE
         </h2>
 
         <p className="text-sm sm:text-base md:text-lg text-gray-300 leading-relaxed text-center max-w-2xl mx-auto">
